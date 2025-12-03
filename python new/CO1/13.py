dd947c41328e58dfb468b98c6f52b52bd17f041b
albinsaji1100 <albinsaji1100@gmail.com>
2025-12-03 11:23:11 +0530

    Add files via upload

---
+++ python new/CO1/13.py
@@ -0,0 +1,9 @@
+n = int(input("Enter an integer n: "))
+           # Compute nn by multiplying n by 11
+nn = n * 11
+n=int(input("Enter a number : "))
+s=(n+11*n+111*n)
+nnn = n * 111 
+s = n + nn + nnn 
+print(f"{n}+{nn} + {nnn} = {s}")
+print(f"{n}+{nn} + {nnn} = {s}")
---
+++ python new/CO1/3.py
@@ -0,0 +1,18 @@
+list1=[2,4,-6,-8,9,7]
+list2=[i for i in list1 if(i>0)]
+print(f"positive  list={list2}")
+
+
+list1=[1,2,3,4,5,6]
+list2=[i**2 for i in list1]
+print(list2)
+
+
+word=input("enter a string:")
+listVowel=[i for i in word if i in 'aeiouAEIOU']
+print(f"vowels are {listVowel}")
+
+
+w=input("enter any string:")
+OrdinalVal=[ord(i) for i in w]
+print(OrdinalVal)
